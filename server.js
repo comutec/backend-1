293e21e83cbf801788805ee125350d076a36300a
Antony Faris <acp325@gmail.com>
2025-05-24 18:11:37 -0500

    express ejercicio

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from the API!" });
 });
 
+app.get("/api/leer", (req, res) => {
+  // Lee el archivo json llamado counter.json
+  res.json({ count: 0 });
+});
+
+app.get("/api/incrementar", (req, res) => {
+  // Actualiza el archivo counter.json
+  res.send("Incrementado.");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
