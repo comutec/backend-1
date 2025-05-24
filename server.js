d7e0a4cbc365fd661058a7c69bcde9f644fa0031
Antony Faris <acp325@gmail.com>
2025-05-24 18:19:40 -0500

    mostrar url

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get("/api/incrementar", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on http://localhost:3000");
 });
