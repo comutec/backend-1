f4e46dac5c8d75d072ffbd5da3c55d76c51f8cd5
Antony Faris <acp325@gmail.com>
2025-05-24 16:08:08 -0500

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+let num = 1;
+const num2 = 2;
+
+if ("1" === 1) {
+  num = 3;
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+function test() {
+  let num = 4;
+  return num;
+  console.log(num);
+}
+
+test();
