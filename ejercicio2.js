ff3aa2117df50720b909bbc455a151ddc313ed08
Antony Faris <acp325@gmail.com>
2025-05-24 17:07:58 -0500

    ejercicio2 solucion

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,2 +1,9 @@
 // leer el archivo persona.json e imprimir los datos como se muetra, sin modificar el contenido del archivo json.:
 // Usuario de nombre Juan y edad 30 vive en Madrid
+import { readFileSync} from "node:fs";
+
+const fileContent = readFileSync("./fixtures/persona.json", "utf-8");
+
+const jsonData = JSON.parse(fileContent);
+
+console.log( "Usuario de nombre " + jsonData.Nombre + " y edad " + jsonData.Edad + " vive en " + jsonData.Ciudad);
