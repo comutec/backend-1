31c86c8e4dc70fb56cc6254509200c51fcb74ae6
Antony Faris <acp325@gmail.com>
2025-05-24 17:20:25 -0500

    ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,16 @@
+// Crea el archivo items.json usando writeFileSync
+// dentro crea 10 items con id de 1 a 10 y un texto aleatorio
+// Ejemplo:
+// {
+//   "items": [
+//     {
+//       "id": 1,
+//       "text": "f4n8m2t9o1"
+//     },
+//     {
+//       "id": 2,
+//       "text": "f4n8m2t9o2"
+//     },
+//     ...
+//   ]
+// Tip: Usa const randomString = Math.random().toString(36).slice(2);
