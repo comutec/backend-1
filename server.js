c43b1e5b4552cbb2326f77eeec1efa403d069c04
Antony Faris <acp325@gmail.com>
2025-05-24 18:08:28 -0500

    express json sample

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the API!" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
